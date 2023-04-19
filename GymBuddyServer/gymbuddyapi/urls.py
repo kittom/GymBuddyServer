0f19bfaf0a6baa0985bb597e2337b53eda59c8df
kittom <39012037+kittom@users.noreply.github.com>
2023-04-19 17:39:38 +0100

    Finalised version of backend for presentation

--- GymBuddyServer/gymbuddyapi/urls.py
+++ GymBuddyServer/gymbuddyapi/urls.py
@@ -9,6 +9,14 @@ urlpatterns = [
     path('workouts/', views.WorkoutCreate.as_view()), 
     path('workouts/<int:account>/', views.WorkoutList.as_view()), 
     path('exercises_by_workout/<int:workout_id>/', views.ExerciseByWorkoutView.as_view()),
-    path('workout/<int:pk>', views.WorkoutDetail.as_view())  
-
+    path('workout/<int:pk>', views.WorkoutDetail.as_view()),
+    path('search/', views.search_accounts, name='search_accounts'),
+    
+    path('friend/request/', views.FriendRequest.as_view(), name='friend-request'),
+    path('friends/<int:pk>/', views.FriendRetrieveUpdateDestroyView.as_view(), name='friend_retrieve_update_destroy'),
+    path('friends/', views.FriendListCreateView().as_view(), name="all friends"),
+    path('posts/', views.PostListCreateView.as_view(), name='post_list_create'),
+    path('posts/<int:pk>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post_retrieve_update_destroy'),
+    path('likes/', views.LikeListCreateView.as_view(), name='like_list_create'),
+    path('likes/<int:pk>/', views.LikeRetrieveUpdateDestroyView.as_view(), name='like_retrieve_update_destroy'),
 ]
